1ad01b14c1182fff43a3abba5fc0d6053bd4b5d1
azkal <mohazkalarif@gmail.com>
2023-08-17 04:43:39 +0000

    fix metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,42 +1,47 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Toaster } from "@/components/ui/toaster"
- 
-import TopBar from '@/components/layouts/top-bar'
-import FloatingNavbar from '@/components/layouts/floating-navbar'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Toaster } from "@/components/ui/toaster";
 
-const inter = Inter({ subsets: ['latin'] })
+import TopBar from "@/components/layouts/top-bar";
+import FloatingNavbar from "@/components/layouts/floating-navbar";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  applicationName:"UKS",
-  title: 'POSKESTREN MANAGEMENT',
-  description: 'management for poskestren amtsilati',
+  applicationName: "UKS",
+  title: "POSKESTREN MANAGEMENT",
+  description: "management for poskestren amtsilati",
   manifest: "/manifest.json",
-  themeColor: "#FFFFFF",
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "cyan" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
   appleWebApp: {
     capable: true,
     statusBarStyle: "default",
-    title: 'POSKESTREN MANAGEMENT',
+    title: "POSKESTREN MANAGEMENT",
     // startUpImage: [],
   },
-}
+  icons: [
+    { rel: "icon", url: "/icon-192x192.png" },
+    { rel: "apple-touch-icon", url: "/icon-192x192.png" },
+  ],
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-      <TopBar/>
-      <main className="p-2 pb-14 md:pb-0">{children}
-      
-      </main>
-      <Toaster />
-      <FloatingNavbar />
+        <TopBar />
+        <main className="p-2 pb-14 md:pb-0">{children}</main>
+        <Toaster />
+        <FloatingNavbar />
       </body>
     </html>
-  )
+  );
 }
--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,7 @@
 export default function Loading() {
   // Or a custom loading skeleton component
   return (
-    <div className="absolute inset-0 flex items-center justify-center"> <div class="loader h-10 w-10">
+    <div className="absolute inset-0 flex items-center justify-center"> <div className="loader h-10 w-10">
     <svg className="circular" viewBox="25 25 50 50">
       <circle className="path" cx="50" cy="50" r="20" fill="none" stroke-width="5" stroke-miterlimit="10"/>
     </svg>
